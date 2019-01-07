fe0be71752b45f87b5bfb146b2221d083f127121
pravar <pravar@localhost.localdomain>
2019-01-07 19:42:23 +0530

    Code changes...

--- jaeger_trace/redis_display.py
+++ jaeger_trace/redis_display.py
@@ -7,37 +7,44 @@ from opentracing.propagation import Format
 import json
 import requests
 import os
+from flask import jsonify
+
+
 
 
 app = Flask(__name__)
 tracer = init_tracer('redis-display')
 redis_host = str(os.getenv('REDIS_HOST'))
 redis_port = str(os.getenv('REDIS_PORT'))
-conn_redis = redis.StrictRedis(host=redis_host, port=6379, db=0)
-#conn_redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+#conn_redis = redis.StrictRedis(host=redis_host, port=6379, db=0)
+conn_redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
 
 @app.route('/display')
 def display_values():
-	
 	span_ctx = tracer.extract(Format.HTTP_HEADERS, request.headers)
 	span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
-
 	with tracer.start_active_span('redis-display-span', child_of=span_ctx, tags=span_tags):
-		print(type(conn_redis.get('Delivery-Guy')))
-		print(type(conn_redis.get('item_ordered')))
-		keys = conn_redis.keys()
-		for i in keys
+		
+		delv_guy = conn_redis.get('Delivery_Guy')
+		order_item = conn_redis.get('Order-Item')
+		order_id = request.headers.get('Order-Id')
+		
+		order_info = {'Order-Id': order_id,
+						'Delivery_Guy': delv_guy.decode('utf-8'),
+						'Order-Item': order_item.decode('utf-8')}
+		print(order_info)
+		#dupe_list=[i.decode('utf-8') for i in key_list]
 
-		return "hello world"
+		return jsonify(order_info)
+# # request for stored information
+# @app.route('/order-info')
+# def get_order_info():
+# 	delivery_guy = requests.args.get('Delivery')
+# 	retv_details = redis.get('delivery_guy')
 
-
-# request for stored information
-@app.route('/order-info')
-def get_order_info():
-	delivery_guy = requests.args.get('Delivery')
-	retv_details = redis.get('delivery_guy')
-
-	return retv_details
+# 	return retv_details
 	
 
 
