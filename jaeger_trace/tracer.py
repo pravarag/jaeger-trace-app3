ea7ac069a75b5f9caecfb814ee5906f49b459ec7
pravar <pravar@localhost.localdomain>
2019-01-08 14:19:54 +0530

    code changed..

--- jaeger_trace/tracer.py
+++ jaeger_trace/tracer.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import logging
@@ -5,6 +6,7 @@ from jaeger_client import Config
 
 
 def init_tracer(service):
+	jaeger_host = os.getenv('JAEGER_AGENT_HOST', 'localhost')
 	logging.getLogger('').handlers = []
 	logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
@@ -13,25 +15,13 @@ def init_tracer(service):
 				'sampler': {
 				'type': 'const',
 				'param': 1,
+				'local_agent':{'reporting_host': jaeger_host},
 			},
 			'logging': True,
 			'reporter_batch_size': 1,
 			},
 			service_name=service,
 
-		)
+		)	
 
 	return config.initialize_tracer()
-
-
-# def call_infinity(input_ob):
-# 	with tracer.start_span('infinite-trace-span') as span:
-# 		hello_str = 'Hello for infinite-trace {}!'.format(input_ob)
-
-# 		for i in range(0, 1000):
-# 			print(hello_str)
-
-# tracer = init_tracer('infinite-trace')
-# call_infinity('new_john')
-# time.sleep(10)
-# tracer.close()	
